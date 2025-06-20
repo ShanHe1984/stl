c7391e851d8ee1bcaebbda5f3d2cf7834d4417a3
ShanHe1984 <56787190+ShanHe1984@users.noreply.github.com>
2025-06-21 00:25:52 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "jervisa"         # 用户名
+UUID = "d6404199-31ed-4ebf-bb82-5a6ae4006913"                     # UUID，留空则自动生成
+PORT = 51813                   # Vmess端口，留空或0则自动生成
+DOMAIN = "ydl.evisa.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiMjU2NzY4MWQxOTJkNGEzOGE1NTM5MjJjM2JkZTI0MzMiLCJ0IjoiMzIxZmRiZjEtM2VmMS00ZWVkLTkxZjQtOTkwZTFlYTMzODc1IiwicyI6Ik9UVmpZVFUwWW1RdFlXRTRaaTAwWlRKbExUa3lOV1V0WTJFeVl6azBNR05pTmpBNSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
